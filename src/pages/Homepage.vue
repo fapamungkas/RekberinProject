be5706f92884acaf7a717638cb2d177a8d3c2936
Fajar Aji Pamungkas <fajar.ajipamungkas1@gmail.com>
2021-11-10 19:12:05 +0700

    update responsive

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -10,7 +10,7 @@
       <div class="col-content md:w-1/2 md:pt-20 pt-12">
         <img class="online-img" src="../assets/image/onlineshop.png" alt="" />
       </div>
-      <div class="col md:w-1/2 md:pt-52 pt-16 pl-3">
+      <div class="col md:w-1/2 md:pt-52 pt-10 px-9">
         <h1 class="text-green-700 text-xl font-bold">#1 Hal yang dilakukan pertama kali</h1>
         <h1 class="text-black font-bold md:text-6xl text-3xl md:pt-6 pt-4">Pembeli Memilih Pesanan</h1>
         <h1 class="text-gray-600 md:text-4xl text-lg md:pt-6 pt-2 md:p-leading">Pembeli memilih pesanan di Facebook kemudian mulai menghubungi admin grup</h1>
@@ -21,7 +21,7 @@
   <!-- Content 2 -->
   <div class="heading">
     <div class="row md:flex">
-      <div class="col md:w-1/2 md:pt-52 pt-36 md:pl-11 pl-3 md:pr-11">
+      <div class="col md:w-1/2 md:pt-52 md:pl-11 md:pr-11 px-9 pt-24">
         <h1 class="text-green-700 text-xl font-bold">#2 Hal yang dilakukan penjual</h1>
         <h1 class="text-black font-bold md:text-6xl text-3xl md:pt-6 pt-4">Memasukkan Detail Pesanan</h1>
         <h1 class="text-gray-600 md:text-4xl text-lg md:pt-6 pt-2 md:p-leading">Setelah mendapatkan kabar dari admin grup, maka penjual membuat detail pesanan pembeli</h1>
@@ -35,10 +35,10 @@
   <!-- Content 3  -->
   <div class="container">
     <div class="row md:flex">
-      <div class="col-content md:w-1/2 pt-20">
+      <div class="col-content md:w-1/2 pt-5 md:pt-24">
         <img src="../assets/image/card-order.png" alt="" />
       </div>
-      <div class="col md:w-1/2 md:pt-72 pt-16 pl-3">
+      <div class="col md:w-1/2 md:pt-72 px-9 pt-4">
         <h1 class="text-green-700 text-xl font-bold">#3 Pembeli melanjutkan pesanan</h1>
         <h1 class="text-black font-bold md:text-6xl text-3xl md:pt-6 pt-4">Membayar Pesanan</h1>
         <h1 class="text-gray-600 md:text-4xl text-lg md:pt-6 pt-2 md:p-leading">Detail pesanan telah selesai dan dapat dilanjutkan ke proses pembayaran</h1>
@@ -49,7 +49,7 @@
   <!-- Content 4 -->
   <div>
     <div class="row md:flex">
-      <div class="col md:w-1/2 md:pt-52 pt-36 md:pl-11 pl-3 md:pr-11">
+      <div class="col md:w-1/2 md:pt-52 pt-20 md:pl-11 md:pr-11 px-9 pt-24">
         <h1 class="text-green-700 text-xl font-bold">#4 Paket dalam perjalanan</h1>
         <h1 class="text-black font-bold md:text-6xl text-3xl md:pt-6 pt-4">Penjual Mengirim Pesanan</h1>
         <h1 class="text-gray-600 md:text-4xl text-lg md:pt-6 pt-2">Pembayaran terkonfirmasi dan pesananpun dikirimkan oleh penjual</h1>
@@ -63,10 +63,10 @@
   <!-- Content 5 -->
   <div>
     <div class="row md:flex">
-      <div class="col-content md:w-1/2 pt-20">
+      <div class="col-content md:w-1/2 md:pt-7 pt-2">
         <img src="../assets/image/friends.png" alt="" />
       </div>
-      <div class="col md:w-1/2 md:pt-72 md:pl-11 pl-3">
+      <div class="col md:w-1/2 md:pt-48 md:pl-11 px-9 pt-xl">
         <h1 class="text-green-700 text-xl font-bold">#5 Barangpun sampai</h1>
         <h1 class="text-black font-bold md:text-6xl text-3xl md:pt-6 pt-4">Pembeli Menerima Barang</h1>
         <h1 class="text-gray-600 md:text-4xl text-lg md:pt-6 pt-2 pb-20">Setelah pembeli mengkonfirmasi menerima barang uangpun dikirimkan kepada penjual</h1>
