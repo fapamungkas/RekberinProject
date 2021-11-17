7b315854e1638c7016696dc1f8e95ccf859b9e9f
Fajar Aji Pamungkas <fajar.ajipamungkas1@gmail.com>
2021-11-17 15:28:22 +0700

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,9 +12,10 @@
       </p>
     </div>
   </div>
+
   <!-- Fitur -->
-  <div id="container">
-    <div class="row flex flex-col md:flex-row">
+  <!-- <div id="container">
+    <div class="row flex flex-col md:flex-row fitur">
       <div class="col md:w-1/2">
         <h1 class="heading-satu md:text-8xl text-5xl md:text-gray-600 font-bold pt-14 md:pl-11 pl-28">Feature</h1>
         <h1 class="heading-dua font-bold md:text-5xl text-3xl text-black pt-12 pl-11">Rekberin menunjang dalam bisnis anda</h1>
@@ -39,10 +40,53 @@
         </div>
       </div>
       <div class="col img-space md:space-img flex md:w-1/2 md:bg-green-700 pb-20 pt-20">
-        <img class="md:-pl-10 pl-10" src="../assets/image/grafix.png" alt="" />
+        <img class="md:-ml-16 pl-10" src="../assets/image/grafix.png" alt="" />
       </div>
     </div>
+  </div> -->
+
+  <!-- Feature Section -->
+  <div class="feature w-full flex md:flex-row flex-col">
+    <div class="featureHeading text-center w-full px-8 md:w-1/2 md:pl-24 md:pb-24">
+      <!-- Heading -->
+      <h1 class="primaryFont text-5xl pt-16 font-extrabold text-green-600 md:text-left">Feature</h1>
+      <h3 class="text-2xl font-bold pt-5 text-left md:text-3xl md:pt-10 md:font-bold md:leading-snug pr-10">Rekberin menunjang dalam bisnis anda.</h3>
+
+      <!-- Sub Heading -->
+      <p class="pt-4 text-1xl text-left font-normal md:pt-6 md:text-xl md:pr-16 md:leading-snug text-gray-600 pr-6">Kami akan membantu anda dengan berbagai fitur yang kami berikan.</p>
+
+      <!-- Feature Points -->
+      <div class="mt-6 pb-16">
+        <!-- Items -->
+        <div class="flex h-6 mt-2 pr-24">
+          <img src="../assets/image/Frame.png" alt="checklist" />
+          <p class="text-green-600 pl-3 font-bold">Pelayanan 24 Jam</p>
+        </div>
+        <!-- Items -->
+        <div class="flex h-6 mt-2">
+          <img src="../assets/image/Frame.png" alt="checklist" />
+          <p class="text-green-600 pl-3 font-bold">Laporan Secara Real-Time</p>
+        </div>
+        <!-- Items -->
+        <div class="flex h-6 mt-2">
+          <img src="../assets/image/Frame.png" alt="checklist" />
+          <p class="text-green-600 pl-3 font-bold">Keamanan Dalam Transaksi</p>
+        </div>
+        <!-- Items -->
+        <div class="flex h-6 mt-2">
+          <img src="../assets/image/Frame.png" alt="checklist" />
+          <p class="text-green-600 pl-3 font-bold">Kode Unik Berbeda</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Feature Images -->
+    <div class="featureImage md:w-1/2 bg-green-700">
+      <div class="whitespace w-full md:w-1/3 md:bg-white"></div>
+      <img class="md:pl-8" src="../assets/image/grafix.png" alt="transaction" />
+    </div>
   </div>
+
   <!-- Manfaat -->
   <div id="container">
     <h1 class="heading text-center text-gray-800 text-5xl md:text-7xl font-bold md:pt-20">Manfaat</h1>
@@ -91,10 +135,31 @@ export default {};
   height: 20rem;
 }
 
+.featureImage img {
+  position: inherit;
+  z-index: 2;
+  margin-top: 25px;
+  width: 750px;
+  height: 95%;
+  margin-left: -100px;
+}
+
+.featureImage {
+  height: 780px;
+}
+
 @media screen and (max-width: 768px) {
-  .img-space img {
-    width: 400px;
-    padding-right: 35px;
+  .featureImage img {
+    height: auto;
+    margin: auto;
+    margin-top: 0px;
+    width: 100%;
+  }
+  .featureImage {
+    background: transparent;
+    padding: 20px;
+    height: auto;
+    margin-bottom: 50px;
   }
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,10 +3,9 @@
     <div class="bg-image">
       <div class="font-sans antialiased" id="app">
         <!-- Class Navbar -->
-        <nav class="navbar flex items-center justify-between flex-wrap pl-6 pr-6 bg-transparent p-6 md:pl-24 md:pr-24">
+        <nav class="navbar flex items-center justify-between flex-wrap p-6 bg-transparent md:pl-24 md:pr-24">
           <!-- Brand and Logos -->
           <div class="flex items-center flex-no-shrink mr-6">
-            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"></svg>
             <img src="../assets/image/logorekberin.png" alt="brandlogo" width="48" />
             <h1 class="font-extrabold pl-3 brand-font">Rekberin</h1>
           </div>
